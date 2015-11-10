70073c66700bbe54e6bafb1f53f44bc41c9b7fed
Joechen1210 <joes9630003@gmail.com>
2015-11-10 11:45:32 +0800

    Update cue_point_teat.js

--- test/cue_point_teat.js
+++ test/cue_point_teat.js
@@ -4,4 +4,19 @@ videojs.plugin('pluginDev', function() {
   overlay.className = 'vjs-overlay';
   overlay.innerHTML = "Becoming a plugin developer";
   player.el().appendChild(overlay);
+  
+  player.catalog.getVideo('4601791650001', function(error, video) {
+   player.catalog.load(video);
+        var tt = player.textTracks()[0];
+        tt.oncuechange = function() {
+          if(tt.activeCues[0] !== undefined){
+            var dynamicHTML = "id: " + tt.activeCues[0].id + ", ";
+            dynamicHTML += "text: " + tt.activeCues[0].text + ", ";
+            dynamicHTML += "startTime: " + tt.activeCues[0].startTime + ",  ";
+            dynamicHTML += "endTime: " + tt.activeCues[0].endTime;
+            document.getElementById("insertionPoint").innerHTML += dynamicHTML + "<br/>";
+          }
+        }
+        player.play();
+      });
 });
